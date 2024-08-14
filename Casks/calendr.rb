09629f7766b949446882590536b44155c3168fbd
Carlos Enumo <paker_wreah@hotmail.com>
2024-08-14 17:26:15 +0100

    Fix url path

--- Casks/calendr.rb
+++ Casks/calendr.rb
@@ -2,7 +2,7 @@ cask "calendr" do
   version "v1.12.0"
   sha256 "8cc50e7520869840531b07b36c5e8ec8706bef5102b8581ec733b11c18351e37"
 
-  url "https://github.com/pakerwreah/Calendr/releases/download/v#{version}/Calendr.zip"
+  url "https://github.com/pakerwreah/Calendr/releases/download/#{version}/Calendr.zip"
   name "Calendr.app"
   homepage "https://github.com/pakerwreah/Calendr"
 
