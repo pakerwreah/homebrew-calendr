880941a5b582a22e19a9822145ca1e18c4d4a68d
Carlos Enumo <paker_wreah@hotmail.com>
2024-08-14 14:55:15 +0100

    Create calendr.rb

---
+++ calendr.rb
@@ -0,0 +1,10 @@
+cask "calendr" do
+  version "replace"
+  sha256 "replace"
+
+  url "https://github.com/pakerwreah/Calendr/releases/download/v#{version}/Calendr.zip"
+  name "Calendr.app"
+  homepage "https://github.com/pakerwreah/Calendr"
+
+  app "Calendr.app"
+end
