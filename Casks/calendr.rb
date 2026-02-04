f02a7001b716dd088041d6c75424882906ff868a
pakerwreah <803954+pakerwreah@users.noreply.github.com>
2026-02-04 22:33:19 +0000

    Update Calendr to version v1.20.2

--- Casks/calendr.rb
+++ Casks/calendr.rb
@@ -1,6 +1,6 @@
 cask "calendr" do
-  version "v1.20.1"
-  sha256 "886003b231087ca872afcb474d97fd93257d650e37cd53a6febbfb4c6141bf6a"
+  version "v1.20.2"
+  sha256 "9776446115b8ff5efe0f1ac56ca2cf1a467e71edd2b34eb264161be150c677e6"
 
   url "https://github.com/pakerwreah/Calendr/releases/download/#{version}/Calendr.zip"
   name "Calendr.app"
